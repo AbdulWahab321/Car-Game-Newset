0274693a3c0eaa4d58d2c7ab8e9e4de6b8e5ced0
AbdulWahab321 <76244094+AbdulWahab321@users.noreply.github.com>
2021-03-22 10:45:06 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,44 @@
+class Form{
+    constructor(){
+       
+    }
+    Hide(){
+
+    }
+    display(){
+        title=createElement("h2");
+        fill("white")
+        title.html("Car Racing Game")
+        title.position(200,-8)
+         input=createInput("Name","text")
+         input.size(150,30)
+         buttun=createButton("OK")
+         buttun.size(50,30)
+        input.position(330,160)
+        buttun.position(450,200)
+        buttun.mousePressed(()=>{
+            
+           
+           
+            
+            pcount+=1
+    
+            player.getName();
+            player.updateName();
+            
+           playerCount+=1
+            
+            
+            player.updateCount();
+            
+            input.hide()
+            buttun.hide()
+            gameState=INS
+          
+            
+            
+            
+        })
+    }
+
+}
